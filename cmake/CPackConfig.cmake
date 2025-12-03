f021976f62124cb806594b023fc64e72ccb2cd90
Ziad Elshereif <ziadkhaled1918@gmail.com>
2025-12-03 16:11:17 +0100

    Add CPack Configuration - Create Debian Package

---
+++ cmake/CPackConfig.cmake
@@ -0,0 +1,12 @@
+set(CPACK_PACKAGE_VENDOR "Ziad Elshereif")
+set(CPACK_PACKAGE_CONTACT "elshereifziad@gmail.com")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "CPack Exercise Example")
+set(CPACK_PACKAGE_HOMEPAGE_URL "https://github.com/Ziaad-Khaled/cpack-exercise-wt2526")
+set(CPACK_PACKAGE_MAINTAINER "Ziad Elshereif")
+
+set(CPACK_GENERATOR "TGZ;DEB")
+
+# Debian specific settings
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Ziad Elshereif")
+set(CPACK_DEBIAN_FILE_NAME "DEB-DEFAULT")
+set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
